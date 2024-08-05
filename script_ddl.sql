6180d0bb606e9dc7c66441cb0d98cce61760a208
tute3e3 <129322687+tute3e3@users.noreply.github.com>
2024-08-05 20:01:19 -0300

    Add files via upload

---
+++ script_ddl.sql
@@ -0,0 +1,88 @@
+-- SCRIPT DDLs "Consultora Big Data"
+
+-- Creacion BD
+CREATE DATABASE bigdata;
+
+-------------------------
+---- Creacion Tablas ----
+
+CREATE TABLE cliente (
+    idCliente int PRIMARY KEY,
+    nombre varchar(50),
+    email varchar(50),
+    empresa int,
+    FOREIGN KEY (empresa) REFERENCES empresa(idEmpresa)
+);
+
+CREATE TABLE empresa (
+    idEmpresa int PRIMARY KEY,
+    nombre varchar(50),
+    pais varchar(50),
+    area varchar(50)
+);
+
+CREATE TABLE servicio (
+    idServicio int PRIMARY KEY,
+    descripcion varchar(100),
+    tecnologia int,
+    cliente int,
+    FOREIGN KEY (tecnologia) REFERENCES tecnologia(idTecnologia),
+    FOREIGN KEY (cliente) REFERENCES cliente(idCliente)
+);
+
+CREATE TABLE tecnologia (
+    idTecnologia int PRIMARY KEY,
+    nombre varchar(50)
+);
+
+CREATE TABLE proyecto (
+    idProyecto int PRIMARY KEY,
+    nombre varchar(50),
+    descripcion varchar(100),
+    horasTotales int,
+    servicio int,
+    empleado int,
+    FOREIGN KEY (horasTotales) REFERENCES facturacion(idFacturacion),
+    FOREIGN KEY (servicio) REFERENCES servicio(idServicio),
+    FOREIGN KEY (empleado) REFERENCES empleado(idEmpleado)
+);
+
+CREATE TABLE servicio_proyecto (
+    id int PRIMARY KEY,
+    idServicio int,
+    idProyecto int,
+    FOREIGN KEY (idServicio) REFERENCES servicio(idServicio),
+    FOREIGN KEY (idProyecto) REFERENCES proyecto(idProyecto)
+);
+
+CREATE TABLE facturacion (
+    id int PRIMARY KEY,
+    monto decimal(10,2),
+    descripcion varchar(100),
+    fecha date
+);
+
+CREATE TABLE empleado (
+    idEmpleado int PRIMARY KEY,
+    nombre varchar(50),
+    email varchar(50),
+    edad decimal(2,0),
+    rol int,
+    FOREIGN KEY (rol) REFERENCES roles(idRol)
+);
+
+CREATE TABLE roles (
+    idRol int PRIMARY KEY,
+    nombre varchar(50),
+    descripcion varchar(100)
+);
+
+CREATE TABLE tarea (
+    idTarea int PRIMARY KEY,
+    descripcion varchar(100),
+    fecha date,
+    rol int,
+    empleado int,
+    FOREIGN KEY (rol) REFERENCES roles(idRol),
+    FOREIGN KEY (empleado) REFERENCES empleado(idEmpleado)
+);
